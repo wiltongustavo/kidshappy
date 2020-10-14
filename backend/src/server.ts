238c6e332a088ccd1ca134bcc390a18801453589
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2020-10-14 15:31:48 -0300

    atualizacao do projeto

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
+import 'express-async-errors';
+import './database/connection'
+import routes from './routes';
+import errorHandler from './errors/handler';
+import cors from 'cors';
 
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.json({message: 'Hellow Word'})
-});
-
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 app.listen(3333);
 
